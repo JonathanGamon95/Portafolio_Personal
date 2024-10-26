bf4346d9f302fc10093c85e6f18a8ac47ef884b4
Jonathan Gamon <jonathaneg51995@gmail.com>
2024-10-26 17:47:14 -0300

    mi portafolio 2.0

---
+++ JS/ANIMACION SCROLL MENU/animacion_scroll.js
@@ -0,0 +1,42 @@
+// botones
+const ABOUT_ME_BOTON = document.getElementById("about_me_boton");
+const PORTAFOLIO_BOTON = document.getElementById("portafolio_boton");
+const CONTACT_FORM_BOTON = document.getElementById("contact_form_boton");
+
+
+const ABOUT_ME = document.getElementById("SOBRE_MI");
+const PORTAFOLIO = document.getElementById("PORTAFOLIO");
+const CONTACT_FORM = document.getElementById("FORMULARIO_DE_CONTACTO")
+
+
+
+
+
+
+const scrollBoton = (elemento) => {
+    const target = document.querySelector(elemento);
+    if (target) {
+        const top = target.getBoundingClientRect().top + window.scrollY; // Obtiene la posición del elemento
+        window.scrollTo({
+            top: top,
+            behavior: 'smooth' // Desplazamiento suave
+        });
+    }
+};
+
+
+ABOUT_ME_BOTON.addEventListener('click', () => {scrollBoton('#SOBRE_MI');});
+PORTAFOLIO_BOTON.addEventListener('click', () => {scrollBoton('#PORTAFOLIO');});
+CONTACT_FORM_BOTON.addEventListener('click', () => {scrollBoton('#FORMULARIO_DE_CONTACTO');});
+
+
+// const inicio = 0;
+// const scroll_0 = (valor) => {
+//     window.scrollTo({
+//         top: valor,
+//         behavior: 'smooth'
+//     });
+// };
+
+// HEADER_INICIO.addEventListener("click", () => scroll_0(inicio));
+// BOTON_FOOTER.addEventListener("click", () => scroll_0(inicio));
