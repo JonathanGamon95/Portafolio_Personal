599166a38d09dc6ca293f095436cb7fb0847a259
Jonathan Gamon <jonathaneg51995@gmail.com>
2024-12-08 10:45:22 -0300

    actualizacion de CV en descarga

--- JS/DESCARGA_DE_CV/descargar_cv.js
+++ JS/DESCARGA_DE_CV/descargar_cv.js
@@ -2,7 +2,7 @@ const BOTON_DESCARGA_CV = document.getElementById("boton_CV");
 const BOTON_DESCARGAR_CV_MOBILE = document.getElementById("boton_CV_mobile");
 
 const descarga = () => {
-    const filePath = "./JS/DESCARGA_DE_CV/CV/desarrollo_web.pdf" // Cambia esto a la ruta correcta
+    const filePath = "./JS/DESCARGA_DE_CV/CV/CV_3.0.pdf" // Cambia esto a la ruta correcta
     const link = document.createElement('a');
     link.href = filePath;
     link.download = 'archivo.pdf'; // Nombre sugerido para el archivo
